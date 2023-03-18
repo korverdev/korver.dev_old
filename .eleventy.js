5094538aead80806815a61b65eb5debc3c49717d
Casey Korver <casey@korver.dev>
2023-03-18 10:27:05 -0500

    Configure webc

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(pluginWebc);
+
     return {
         dir: {
             input: "src"
