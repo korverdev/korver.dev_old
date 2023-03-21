6a088f16b05ca9e4ad25310078fd37e0c506b176
Casey Korver <84342833+Casey-Kiewit@users.noreply.github.com>
2023-03-20 21:10:07 -0500

    Complete Folder Layout & Build Process (#10)

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
+const root_folder = "src"
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(pluginWebc);
+    eleventyConfig.addPlugin(pluginWebc, {
+        components: root_folder + "/_includes/components/**/*.webc"
+    });
 
     return {
         dir: {
-            input: "src",
-            layouts: "_layouts"
+            input: root_folder
         },
         htmlTemplateEngine: "webc",
         markdownTemplateEngine: "webc"
